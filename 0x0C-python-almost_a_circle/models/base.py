d9dc4938241644281915d56d9edadbf416d1c4cb
mourgh21 <mourgh21@gmail.com>
2024-02-23 14:44:25 -0800

    DONE

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Base:
+    """Base class for managing id attribute.
+
+    Attributes:
+        __nb_objects (int): Counter for number of instances.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes Base instance with an optional id."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
