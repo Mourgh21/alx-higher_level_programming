4cb0f61d9ea802288b9fb0042ae896fdc7caa84d
Abdessamad Mourgh <mourgh21@gmail.com>
2024-04-22 23:31:48 +0000

    DONE

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,25 +1,43 @@
 #!/usr/bin/node
 
-// This line specifies the path to the interpreter for the script.
+const arg = process.argv.slice(2);
 
-if (process.argv.length <= 3) {
-  console.log('0');
-  // If there are not enough command-line arguments (less than or equal to 3),
-  // this line outputs '0' to the console.
-} else {
-  // If there are enough command-line arguments, this block of code executes.
-
-  const arr = process.argv.slice(2).map(Number);
-  // This line creates an array "arr" by slicing the command-line arguments,
-  // starting from index 2 and then converting each element to a number.
-
-  const second = arr.sort(function (a, b) { return b - a; })[1];
-  // This line sorts the "arr" array in descending order and selects the second,
-  // element(index 1) from the sorted array.
-  // This effectively finds the second largest number among the provided,
-  // command - line arguments.
-
-  console.log(second);
-  // This line outputs the value of the "second" variable,
-  // (second largest number) to the console.
+const array = [];
+for (let i = 0; i < arg.length; i++) {
+  const num = Number(arg[i]);
+  array.push(num);
 }
+
+function print2Biggest (arr, arrSize) {
+  let i;
+  let biggest = -2454635434;
+  let second = -2454635434;
+
+  if (arrSize < 2) {
+    console.log(0);
+    return;
+  }
+
+  // Find the biggest element
+  for (i = 0; i < arrSize; i++) {
+    if (arr[i] > biggest) {
+      biggest = arr[i];
+    }
+  }
+
+  // Find the second biggest element
+  for (i = 0; i < arrSize; i++) {
+    if (arr[i] > second && arr[i] < biggest) {
+      second = arr[i];
+    }
+  }
+
+  if (second === -2454635434) {
+    console.log(0);
+  } else {
+    console.log(second);
+  }
+}
+
+const n = array.length;
+print2Biggest(array, n);
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,22 +1,11 @@
 #!/usr/bin/node
 
-// This line specifies the path to the interpreter for the script.
+const arg = process.argv.slice(2);
 
-if (process.argv.length === 2) {
-  // This condition checks if there are exactly 2
-  // elements in the process.argv array.
-  // If true, it means no additional command-line arguments were provided
-  // besides the script name.
+if (arg.length === 0) {
   console.log('No argument');
-  // Outputs the string 'No argument' to the console.
-} else if (process.argv.length === 3) {
-  // This condition checks if there are exactly 3 elements in the process.argv.
-  // If true, it means one additional command-line argument was provided.
+} else if (arg.length === 1) {
   console.log('Argument found');
-  // Outputs the string 'Argument found' to the console.
 } else {
-  // If neither of the above conditions is met, it means
-  // more than one additional command-line argument was provided.
   console.log('Arguments found');
-  // Outputs the string 'Arguments found' to the console.
 }
